fb12d573793919397962a70442bfd53b91470666
Petya Babukova <p_babukova@abv.bg>
2022-03-01 18:40:52 +0200

    add about controller

--- routes.js
+++ routes.js
@@ -1,14 +1,18 @@
 const {Router} = require ('express');
-
-// const express = require('express'); // this is equal on 1 line.
+// const express = require('express'); // next 3 lines are equal on 1 line.
 // const router = express.Router;
 //const Router = require ('express').Router;
 
+const productController = require('./controllers/productCotroller');
+const aboutController = require('./controllers/aboutController');
 
 const router = Router();
 
-router.get('/', (req, res)=>{
-    res.render('home', {layout: false})
-});
+router.use('/', productController) //('/products', productController) - това е по-добрия вариант
+// router.get('/', productController.index); //Това е олдфешън начина. Ивчо не го харесва, защото отговорността за раута е концентрирана само на едно място, за всеки раут може да има много екшъни и не е добре това. По добре да изнесем раута в контролера. 
+// router.get('/create', productController.create);
+
+router.use('/about', aboutController);
+
 
 module.exports = router;
